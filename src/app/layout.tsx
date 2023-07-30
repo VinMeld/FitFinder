edcd45bf711cd48c45aec10d95647bcaeae7b89c
RohanSreelesh <31416057+RohanSreelesh@users.noreply.github.com>
2023-07-30 16:43:32 -0400

    added umami script

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -31,9 +31,9 @@ export default async function RootLayout({
   } = await supabase.auth.getSession();
   return (
     <html lang="en" className={poppins.className}>
-      <Head> 
+      <head> 
       <Script async src="https://analytics.umami.is/script.js" data-website-id="bb60014a-bdf3-4a39-a7de-af5e39de1301"></Script>
-      </Head>
+      </head>
       <body className="flex flex-col min-h-screen">
         <SupabaseProvider>
           <SupabaseAuthProvider serverSession={session}>
