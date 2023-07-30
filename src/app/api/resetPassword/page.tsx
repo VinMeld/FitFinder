82eafe552d525771af7cde10120e5bb6a0dda539
vinaycat <vinaymeldrum@gmail.com>
2023-07-30 11:43:09 -0400

    Changing format

--- src/app/api/resetPassword/page.tsx
+++ src/app/api/resetPassword/page.tsx
@@ -11,9 +11,10 @@ export default function ResetPassword() {
   const searchParams = useSearchParams()!
   //store in state
   const [token, setToken] = useState('');
-  const [code, setCode] = useState('');
   const [type, setType] = useState('');
   const [email, setEmail] = useState('');  // new email state
+  const [otp, setOtp] = useState('');  // new otp state
+  const [code, setCode] = useState('');  // new code state
 
   type EmailOtpType = 'signup' | 'invite' | 'magiclink' | 'recovery' | 'email_change' | 'email'
 
@@ -42,7 +43,7 @@ export default function ResetPassword() {
     //   toast.error("using code to get session");
     //   console.log(data, error)
     // }
-    const { error:verifyError } = await supabase.auth.verifyOtp({ token, type })
+    const { error:verifyError } = await supabase.auth.verifyOtp({ email, type: type as EmailOtpType, token:otp})
     if (verifyError) {
         console.error(verifyError);
         toast.error("Oops there was an error!");
@@ -58,26 +59,41 @@ export default function ResetPassword() {
         toast.success("Password changed successfully!");
         setPassword('');
         setConfirmPassword('');
+        setOtp('');
+        setType('');
+        setToken('');
+        setEmail('');
+        setCode('');
     }
   }
 
   return (
     <>
     <ToastContainer />
-    <form onSubmit={onSubmit}>
-           <>
-        <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Enter Your New Password</h3>
-        <div>
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your new password</label>
-            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" onChange={(e) => setPassword(e.target.value)} required />
-        </div>
-        <div>
-            <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm your new password</label>
-            <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" onChange={(e) => setConfirmPassword(e.target.value)} required />
-        </div>
-        <button type="submit" className="w-full mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Reset Password</button>
-    </>
-    </form>
+    <div className="flex items-center justify-center w-4/5 h-auto bg-black">
+      <form onSubmit={onSubmit}>
+            <>
+          <h3 className="mb-4 text-xl font-medium text-gray-900 dark:text-white">Enter Your New Password</h3>
+          <div>
+              <label htmlFor="email" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your Email</label>
+              <input type="email" name="email" id="email" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" onChange={(e) => setEmail(e.target.value)} required />
+          </div>
+          <div>
+              <label htmlFor="otp" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your OTP</label>
+              <input type="text" name="otp" id="otp" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" onChange={(e) => setOtp(e.target.value)} required />
+          </div>
+          <div>
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Your new password</label>
+              <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" onChange={(e) => setPassword(e.target.value)} required />
+          </div>
+          <div>
+              <label htmlFor="password" className="block mb-2 text-sm font-medium text-gray-900 dark:text-white">Confirm your new password</label>
+              <input type="password" name="password" id="password" placeholder="••••••••" className="bg-gray-50 border border-gray-300 text-gray-900 text-sm rounded-lg focus:ring-blue-500 focus:border-blue-500 block w-full p-2.5 dark:bg-gray-600 dark:border-gray-500 dark:placeholder-gray-400 dark:text-white" onChange={(e) => setConfirmPassword(e.target.value)} required />
+          </div>
+          <button type="submit" className="w-full mt-4 text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:outline-none focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800">Reset Password</button>
+      </>
+      </form>
+    </div>
     </>
   )
 }
