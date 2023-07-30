9c62b6ceb028fd74ae2cfe159f30885b5d38751c
vinaycat <vinaymeldrum@gmail.com>
2023-07-30 14:02:08 -0400

    Fixed webiste length

--- src/app/profile/edit/trainer/page.tsx
+++ src/app/profile/edit/trainer/page.tsx
@@ -249,7 +249,7 @@ export default function TrainerEdit() {
                           type="text"
                           name="website"
                           id="website"
-                          maxLength={30}
+                          maxLength={50}
                           class="block w-full p-4 text-gray-900 border border-gray-300 rounded-lg bg-gray-50 sm:text-md focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                           placeholder="www.johndoe.com"
                         />
